2d81150aa462f889f608034f249a048f6e4a8e54
Bob <151000427+WilkBob@users.noreply.github.com>
2024-06-27 17:23:26 -0700

    chore: Update Vite config trying to resolve image paths

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,11 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     base: "./",
-
-    });
+    //images are at '/images', js and css are at /assets. I set root here for the js and css, but images need a diffrent root
+    
+resolve: {
+    alias: {
+      '@images': path.resolve(__dirname, '/images')
+    },
+    },
+});
